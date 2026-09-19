52930eb5b50506f51b8f2b2483f02a882b1db117
Mykhailo <mcmish133@gmail.com>
2026-09-19 15:41:56 +0300

    add core entities (User, Group, GroupMember, Message)

---
+++ src/ChatPlatform.Server/Entities/Message.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ChatPlatform.Server.Entities
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid SenderId { get; set; }
+        public User? Sender { get; set; }
+        public Guid GroupId { get; set; }
+        public Group? Group { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset SentAt { get; set; }
+
+        public Message(Guid senderId, Guid groupId, string text)
+        {
+            Id = Guid.NewGuid();
+            SenderId = senderId;
+            GroupId = groupId;
+            Text = text;
+            SentAt = DateTimeOffset.UtcNow;
+        }
+    }
+}
